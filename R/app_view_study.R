fb9133a15ee7232836ab4cd129a830cb8805c827
c5sire <rsimon64@gmail.com>
2015-03-06 10:21:56 -0500

    Added first widget

---
+++ R/app_view_study.R
@@ -0,0 +1,26 @@
+library(shiny)
+
+#' app_view_study
+#'
+#' Creates a shiny widget to display the study in tabular form.
+#' 
+#' @param data a data.frame
+#' @return character a string; default is 'Hello, world!'
+#' @author Reinhard Simon
+#' @family study
+#' @export
+app_view_study <- function(data) {
+  #stopifnot(is.character(name))
+  
+  shinyApp(
+    ui = fluidPage( 
+      dataTableOutput('mytable1')
+    ),
+    server = function(input, output) {
+      output$mytable1 <- renderDataTable({
+        data # this gets the data.frame
+      }, options = list(lengthMenu = c(5, 20, 50), pageLength = 20))
+    },
+    options = list(height = 500)
+  )
+} 
---
+++ tests/testthat/test_get_study.R
@@ -0,0 +1,8 @@
+context("get_study")
+
+test_that("get_study function operates well", {
+  # The following function should be used to reduce time on CRAN by avoiding running a test since time on CRAN is limited.
+  # skip_on_cran()
+  
+  #expect_that(get_study() == "Hello, world!", is_true())
+}) 
