e3e525bd36dfa4097b41c63080f038fe4733619c
c5sire <rsimon64@gmail.com>
2015-05-21 13:29:19 -0500

    add docs

--- R/import_datacollector.R
+++ R/import_datacollector.R
@@ -8,6 +8,24 @@ get_minimal_meta <- function (minimal) {
   list(id = id, crop = crop, year= year, locality =locality)
 }
 
+prepend_meta <- function(db, mm){
+  n <- nrow(db)
+  pre <- cbind(id  = rep(mm$id, n),
+               crop = rep(mm$crop, n),
+               program = rep("", n),
+               year = rep(mm$year, n),
+               locality = rep(mm$locality, n)
+  )
+  
+  
+  adf <- cbind(pre, db)
+  adf[, 1] <- as.character(adf[, 1])
+  adf[, 2] <- as.character(adf[, 2])
+  adf[, 3] <- as.character(adf[, 3])
+  adf[, 4] <- as.integer(as.character(adf[, 4]))
+  adf[, 5] <- as.character(adf[, 5])
+  adf
+}
 
 
 import_datacollector <- function(filepath){
@@ -15,25 +33,6 @@ import_datacollector <- function(filepath){
   minimal <- readxl::read_excel(filepath, "Minimal")
   mm <- get_minimal_meta(minimal)
 
-  prepend_meta <- function(db, mm){
-    n <- nrow(db)
-    pre <- cbind(id  = rep(mm$id, n),
-                 crop = rep(mm$crop, n),
-                 program = rep("", n),
-                 year = rep(mm$year, n),
-                 locality = rep(mm$locality, n)
-    )
-    
-    
-    adf <- cbind(pre, db)
-    adf[, 1] <- as.character(adf[, 1])
-    adf[, 2] <- as.character(adf[, 2])
-    adf[, 3] <- as.character(adf[, 3])
-    adf[, 4] <- as.integer(as.character(adf[, 4]))
-    adf[, 5] <- as.character(adf[, 5])
-    adf
-  }
-    
   minimal %>% prepend_meta(mm) %>% post_minimal()
   readxl::read_excel(filepath, "Installation")%>%  prepend_meta(mm) %>% post_installation()
   readxl::read_excel(filepath, "Material List")%>%  prepend_meta(mm) %>% post_material_list()
